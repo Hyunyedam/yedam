ddabbb3d5d24416a4433adf9c090a7c42ecbae0b
현예담 <happy-dam@naver.com>
2021-05-01 16:49:58 +0900

    516545

--- 문제 300/문제 11~20.py
+++ 문제 300/문제 11~20.py
@@ -22,3 +22,29 @@ a = 128
 print( type(a) )
     # int : 정수  # float : 실수
     # str : 문자
+
+# 문제 16 : int("문자") 함수 : 숫자로 변환
+num_str = "720" # 문자
+num_int = int(num_str) # 문자 => 숫자 변환
+
+# 문제 17 : str("숫자") 함수 : 문자로 변환
+num = 100
+num_str = str(num)
+print(num_str)
+
+# 문제 18 : float("문자") 함수 : 실수로 변환
+문자열 = "15.79"
+실수형 = float(문자열)
+print(실수형)
+
+# 문제 19
+year = "2020"
+print(int("2020") +0)
+print(int("2020") +1)
+print(int("2020") +2)
+
+# 문제 20
+에어컨 = 48584
+무이자 = 36
+총금액 = 에어컨 * 무이자
+print(총금액)
