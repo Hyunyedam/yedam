bab63b66b672cd813b7f9702245dce704e5aaded
현예담 <happy-dam@naver.com>
2021-05-01 16:22:27 +0900

    516545

--- 문제 300/문제 1~10.py
+++ 문제 300/문제 1~10.py
@@ -1,2 +1,30 @@
 
 # 문제 1
+print("Hello World")
+
+# 문제 2
+print("Mary's cosmetics")
+
+# 문제 3
+print('"도둑이야".')
+
+# 문제 4
+print('"C:\Windows"')
+
+# 문제 5  \n : 들여쓰기     \t : 띄어쓰기
+print("안녕하세요.\n만나서\t\t반갑습니다.")
+
+# 문제 6  , : 띄어쓰기 역할
+print("오늘은", "일요일")
+
+# 문제 7
+print("naver","kakao","sk","samsung",sep=';')
+
+# 문제 8
+print("naver","kakao","sk","samsung", sep='/')
+
+# 문제 9
+print("first",end='');print("second",end='')
+
+# 문제 10
+print("\n", 5%3)
