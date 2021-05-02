e63c36cffef1646833aaaa2a9f0421f621e69483
현예담 <happy-dam@naver.com>
2021-05-02 15:52:43 +0900

    00

--- 문제 300/문제 41~50.py
+++ 문제 300/문제 41~50.py
@@ -1,7 +1,42 @@
 
-# 문제 41
+# 문제 41 lower 메서드
 ticker = "btc_krw"
 ticker1 = ticker.upper()
 print(ticker1)
 
 # 문제 42
+ticker = "BTC_KRW"
+ticker = ticker.lower()
+print(ticker)
+
+# 문제 43 capitalize 메서드
+a = "hello"
+a = a.capitalize()
+
+# 문제 44 endswith 메서드
+film_name = "보고서.xisx"
+film_name.endswith("xlsx")
+
+# 문제 45 endswith 메서드
+file_name = "보고서.xlsx"
+file_name.endswith(("xlsx", "xls"))
+
+# 문제 46 startswith 메서드
+file_name = "2020_보고서.xlsx"
+file_name.startswith("2020")
+
+# 문제 47 split 메서드
+a= "hello world"
+a.split()
+
+# 문제 48 split 메서드
+ticker = "btc_krw"
+ticker.split("_")
+
+# 문제 49
+date = "2020-05-01"
+date.split("-")
+
+# 문제 50 restrip 메서드
+data = "039490"
+data = data.rstrip()
