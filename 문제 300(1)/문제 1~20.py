0cc188342ad57d86023ae1050b01a3b4c760d267
krsthg <ypseoha@naver.com>
2021-07-03 15:47:04 +0900

    0

---
+++ 문제 300(1)/문제 1~20.py
@@ -0,0 +1,79 @@
+
+# 001   print 기초
+print("Hello World")
+
+# 002   print 기초
+print("Mary's cosmetics")
+
+# 003   print 기초
+print('"도둑이야".')
+
+# 004   print 기초
+print('"C:\Windows')
+
+# 005   print 탭과 줄바꿈
+print("안녕하세요.\n만나서\t\t반갑습니다.")
+# \n : 들여쓰기( 엔터 )  \t : 띄어쓰기( 스페이스 )
+
+# 006   print 여러 데이터 출력   , : 띄어쓰기를 해줌
+print("오늘은","일요일")
+
+# 007   print 기초
+print("naver","kakao","sk","samsung",sep=";")
+
+# 008   print 기초
+print("naver","kakao","sk","samsung",sep="/")
+
+# 009   print 줄바꿈
+print("first",end='');print("second",end='')
+
+# 010   연산 결과 출력
+print("\n",5/3)
+
+# 011   변수 사용하기
+삼성전자 = 50000
+총평가금액 = 삼성전자 * 10
+print(총평가금액)
+
+# 012   변수 사용하기
+시가총액 = 298000000000000
+현재가 = 50000
+PER = 15.79
+
+# 013   문자열 출력
+s = "hello!"
+t = "python"
+print(s,t)
+
+# 014   파이썬을 이용한 값 계산
+print(2+2*3)
+
+# 015   type 함수
+a = 132
+print(type(a))
+
+# 016   문자열을 정수로 변환
+num_str = "720"
+num_int = int(num_str)
+
+# 017
+num = 100
+num_str = str(num)
+print(num_str)
+
+# 018   문자열을 실수로 변환
+문자열 = "15.79"
+실수 = float(문자열)
+print(실수)
+
+# 019   문자열을 정수로 변환
+year = "2020"
+print(int("2020") +0)
+print(int("2020") +1)
+print(int("2020") +2)
+
+# 020   파이썬 계산
+에어컨 = 48584
+무이자 = 36
+총금액 = 에어컨 * 무이자
+print(총금액)
