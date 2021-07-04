3b64a23801456b16eab1a8ede888b027d40470bb
krsthg <ypseoha@naver.com>
2021-07-04 16:54:07 +0900

    0

---
+++ 문제 300(1)/문제 111~120.py
@@ -0,0 +1,33 @@
+
+# 111   문자열 출력하기
+a = '안녕하세요'
+print(a*2)
+
+# 112   숫자 입력받아 출력하기
+a = int(input("숫자를 입력하세요 : "))
+print(a + 10)
+
+# 113   짝수 홀수 판별하기
+a = int(input("짝수/홀수 판별할 숫자를 입력하세요 : "))
+if a/2 == 0:
+    print("짝수")
+else:
+    print("홀수")
+
+# 114   숫자 입력받아 출력하기
+a = int(input('입력값 : '))
+if a > 255:
+    print(255)
+else :
+    print(a + 20)
+
+# 115
+a = int(input("20을 뺄 값을 입력하세요 : "))
+if a-20 > 255 :
+    print(255)
+elif a-20 < 0 :
+    print(0)
+else:
+    print(a-20)
+
+# 116
--- 문제 300(1)/문제 71~80.py
+++ 문제 300(1)/문제 71~80.py
@@ -44,9 +44,3 @@ print(a, b, c)      # apple banana cake 출력
 # 080   range 함수 ( 참고 )
 a = tuple(range(2, 100, 2))
 print(a)
-
-# 081   별 표현식 ( 참고 )
-scores = [8.8, 8.9, 8.7, 9.2, 9.3, 9.7, 9.9, 9.5, 7.8, 9.4]
-print(*scores)
-*scores,_,_ = scores
-print(scores)
